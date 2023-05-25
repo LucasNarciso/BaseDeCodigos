fe233ef126d5745512cf0b3a417a9ff653ae3741
Lucas Narciso <71471494+LucasNarciso@users.noreply.github.com>
2023-05-25 20:16:48 -0300

    Update Consts.js

--- src/Global/Consts.js
+++ src/Global/Consts.js
@@ -1,1 +1,1 @@
-export const baseURL = "https://script.google.com/macros/s/AKfycbyYlbAhxbrpGp-3n2f6tX1xdBnZSusWumlOfBwG4b6PgOGro6BW_uEWbI32vRG21049/exec?"
+export const baseURL = "https://script.google.com/macros/s/AKfycbzkaTtodVeUAGGPTBoHKr_c9EcErmwB6HAJWvtwWgeZXq40biCaF8N2JC8Nj1oo0cKu/exec?"
