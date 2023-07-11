4a942406b53209f38a87d727e0c19bb3cf346ead
LucasNarciso <lucasnarcisoo@gmail.com>
2023-07-10 22:51:41 -0300

    Voltando url normal, tentativa 1 de arrumar o logoff

--- src/Global/Consts.js
+++ src/Global/Consts.js
@@ -1,2 +1,2 @@
-//export const baseURL = "https://script.google.com/macros/s/AKfycbyt4txR9HwOs98VgT_w9y1IjvHQlCNqQBR2Yu2jcqLZ_rVuEiewSe8b0cuS89gcM_-G/exec"
-export const baseURL = "https://script.google.com/macros/s/AKfycbxgqvTMYWdM9LU7cQQmlNaZqADdTp3XJgtybGeGS7YsisqeGuPuvGAnVR3Ah_MJVEAV/exec"
+export const baseURL = "https://script.google.com/macros/s/AKfycbyt4txR9HwOs98VgT_w9y1IjvHQlCNqQBR2Yu2jcqLZ_rVuEiewSe8b0cuS89gcM_-G/exec"
+//export const baseURL = "https://script.google.com/macros/s/AKfycbxgqvTMYWdM9LU7cQQmlNaZqADdTp3XJgtybGeGS7YsisqeGuPuvGAnVR3Ah_MJVEAV/exec"
